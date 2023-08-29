c99faf9527c1136ff61a319b6f8fadc5655c22d2
July <106986983+July-vilh@users.noreply.github.com>
2023-08-29 22:43:20 +0300

    Create 3. Calling the email registration change method (PUT).py

---
+++ Project1/3. Calling the email registration change method (PUT).py
@@ -0,0 +1,22 @@
+# 3. Calling the email registration change method (PUT)
+
+import requests
+import json
+
+url = "http://5.63.153.31:5051/v1/account/email"
+
+payload = json.dumps({
+  "login": "login_6",
+  "password": "login_66",
+  "email": "login7@mail.ru"
+})
+headers = {
+  'X-Dm-Auth-Token': 'voluptate magna incididunt',
+  'X-Dm-Bb-Render-Mode': 'voluptate magna incididunt',
+  'Content-Type': 'application/json',
+  'Accept': 'text/plain'
+}
+
+response = requests.request("PUT", url, headers=headers, data=payload)
+
+print(response.text)
