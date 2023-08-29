c9ee0c49d5e981579a71243e53cf3d4d60d39c4c
July <106986983+July-vilh@users.noreply.github.com>
2023-08-29 23:16:13 +0300

    Create 7. Calling method for RESET registered password (POST).py

---
+++ Project1/7. Calling method for RESET registered password (POST).py
@@ -0,0 +1,22 @@
+# 7. Calling method for RESET registered password (POST)
+
+import requests
+import json
+
+url = "http://5.63.153.31:5051/v1/account/password"
+
+payload = json.dumps({
+  "login": "login_6",
+  "email": "login7@mail.ru"
+})
+headers = {
+  'X-Dm-Auth-Token': 'voluptate magna incididunt',
+  'X-Dm-Bb-Render-Mode': 'voluptate magna incididunt',
+  'Content-Type': 'application/json',
+  'Accept': 'text/plain'
+}
+
+response = requests.request("POST", url, headers=headers, data=payload)
+
+print(response.text)
+
