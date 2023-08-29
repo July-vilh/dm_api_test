f317b8955e55cb99b0e96163ed94473481ffc64b
July <106986983+July-vilh@users.noreply.github.com>
2023-08-29 22:53:49 +0300

    Create 4. Calling the email registration change method (POST).py

---
+++ Project1/4. Calling the email registration change method (POST).py
@@ -0,0 +1,21 @@
+# 4. Calling the email registration change method (POST)
+
+import requests
+import json
+
+url = "http://5.63.153.31:5051/v1/account/login"
+
+payload = json.dumps({
+  "login": "login_6",
+  "password": "login_66",
+  "rememberMe": True
+})
+headers = {
+  'X-Dm-Bb-Render-Mode': 'voluptate magna incididunt',
+  'Content-Type': 'application/json',
+  'Accept': 'text/plain'
+}
+
+response = requests.request("POST", url, headers=headers, data=payload)
+
+print(response.text)
